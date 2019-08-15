eaa3421c3f62561d6d08f5cc2ffaabf5c93ecab0
border-radius <pharshmaster@gmail.com>
2019-08-15 21:48:25 +0300

    Separate format function, fix link formating

---
+++ functions/format.js
@@ -0,0 +1,23 @@
+const htmlToText = require('html-to-text')
+const cheerio = require('cheerio')
+
+module.exports = format
+
+function format(untochedHTML) {
+    // first line in HN comment is not framed as a paragraph,
+    // so htmlToText is not placing line wrap at it's end
+    const fixFirstParagraph = `<p>${untochedHTML.replace('<p>', '</p><p>')}`
+    
+    // set all links text equal to the href
+    // so html-to-text wont create wierd markdown-like links
+    // like "text... [link]"
+    const query = cheerio.load(fixFirstParagraph)
+    query('a').each(function () {
+        query(this).text(query(this).attr('href'))
+    })
+
+    return htmlToText.fromString(query.html(), {
+        wordwrap: null,
+        hideLinkHrefIfSameAsText: true,
+    }).trim()
+}
