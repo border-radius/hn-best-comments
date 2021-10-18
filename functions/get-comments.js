7aa298a81b638b80fa46d68e64dc017ee09375b1
Vadim <pharshmaster@gmail.com>
2021-10-18 22:12:40 +0300

    Fix story title parsing (text)

--- functions/get-comments.js
+++ functions/get-comments.js
@@ -21,7 +21,7 @@ async function getComments() {
             text: format(query(this).find('.comment').html()),
             author: query(this).find('.hnuser').text().trim(),
             story_link: `${URL_PREFIX}/${query(this).find('.onstory a').attr('href')}`,
-            story_title: query(this).find('.storyon a').text().trim(),
+            story_title: query(this).find('.onstory a').text().trim(),
         }
     }).get()
 }
